86733a674844dd3b6ace4099a714acc4e17f85cb
Quinn04-dev <kodyjack7@gmail.com>
2025-12-06 01:35:05 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/twisted-dashboard/",
+  base: "/Quinn04-dev/",
   plugins: [react()],
 });
 
